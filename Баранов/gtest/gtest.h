dfa4a8ee4ea23de95eb3d0c73c93213989a706cb
pavelbar <pavelbaranow@gmail.com>
2016-11-21 20:28:22 +0300

    clean

--- Баранов/gtest/gtest.h
+++ Баранов/gtest/gtest.h
@@ -1,6 +1,6 @@
 // Copyright 2005, Google Inc.
 // All rights reserved.
-//
+//pb
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
