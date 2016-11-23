5fc0afcbdd99d020d1ec35b4f0ecbd0be75187c8
pavelbar <pavelbaranow@gmail.com>
2016-11-23 18:57:50 +0300

    Реализация класса queue + тесты на него

--- Баранов/queue/queue.h
+++ Баранов/queue/queue.h
@@ -1,16 +1,30 @@
-#include <cassert>
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+
+#include <iostream>  //debug
+using namespace std; //debug
+#include <locale>    //debug
+
 const unsigned int MaxSize = 100;
 template <class T>
 class Queue
 {
 private:
-    T *TMEM;   // указатель на очередь
-    unsigned int size;  // максимальное количество элементов в очереди
-    int begin; // начало очереди
-    int end;   // конец очереди
-    unsigned int count; // счетчик элементов
+    T *TMEM;            // указатель на очередь
+    T *TMEMTMP;         // указатель на tmp
+    int size;           // размер очереди
+    int begin;          // начало очереди
+    int end;            // конец очереди
+    int count;          // счетчик элементов
 public:
-    Queue(int = 100);          // конструктор по умолчанию
+    Queue(int = MaxSize);         // конструктор по умолчанию
+    Queue(const Queue<T> & val);  // конструктор копирования
+    ~Queue();                     // деструктор
+    bool IsEmpty();               // очередь пуста
+    bool IsFull();                // очередь полна
+    void enqueue(const T &val);   // добавить элемент в в конец очереди
+    T dequeue();                  // взять первый элемент из очереди
+    void printQueue();            //debug       
 };
 
 
@@ -18,9 +32,89 @@ template <class T>// конструктор по умолчанию
 Queue<T>::Queue(int sizeQueue)
 {
     if ((sizeQueue<1) || (sizeQueue>MaxSize)) throw (sizeQueue);
-    size = sizeQueue; // инициализация константы
+    size = sizeQueue; 
     begin = 0;
     end = 0;
     count = 0;
-    TMEM = new T[size];
+    TMEM = new T[size+1];
+    TMEMTMP = new T[1];
 }
+
+template <class T>// конструктор копиирования
+Queue<T>::Queue(const Queue &otherQueue)
+{
+    size = otherQueue.size;
+    begin = otherQueue.begin;
+    end = otherQueue.end;
+    count = otherQueue.count;
+    TMEM = new T[size + 1];
+    for (int i = 0; i < size; i++)
+        TMEM[i] = otherQueue.TMEM[i]; 
+}
+
+template <class T>// деструктор 
+Queue<T>::~Queue()
+{
+    delete[] TMEM;
+    delete[] TMEMTMP;
+    TMEM = NULL;
+    TMEMTMP = NULL;
+}
+
+template <class T>// очередь пуста
+bool Queue<T>::IsEmpty()
+{
+    return(count == 0);
+}
+
+template <class T>// очередь полна
+bool Queue<T>::IsFull()
+{
+    return (count == size);
+}
+
+template <class T>//добавить элемент в в конец очереди
+void Queue<T>::enqueue(const T &newElem)
+{
+    if (IsFull())  throw (count);
+    TMEM[end] = newElem; 
+    end++;
+    count++;
+
+    if (end > size)
+        end = end - size + 1; 
+}
+
+template <class T>// взять первый элемент из очереди
+T Queue<T>::dequeue()
+{
+    if (IsEmpty()) throw (count);
+    TMEMTMP[0] = TMEM[begin];
+    begin++;
+    count--;
+    if (begin > size)
+        begin -= size + 1; 
+    return TMEMTMP[0];
+}
+
+template<typename T>//debug
+void Queue<T>::printQueue()
+{
+    cout << endl << "Очередь: " << endl;
+    if (count == 0 )
+        cout << " пустая\n";
+    else
+    {
+        int tmp = count;
+        int tmp2 = end;
+        while (tmp != 0)
+        {
+            cout << TMEM[tmp2 - 1] << " ";
+            tmp--;
+            tmp2--;
+        }  
+        cout << endl;
+    }
+}
+
+#endif 
--- Баранов/tests/test_queue.cpp
+++ Баранов/tests/test_queue.cpp
@@ -5,3 +5,115 @@ TEST(Tqueue, can_create_queue_with_positive_length)
 {
     ASSERT_NO_THROW(Queue<int> v(5));
 }
+
+TEST(Queue, cant_create_queue_with_negative_length)
+{
+    ASSERT_ANY_THROW(Queue<int> v(-5));
+}
+
+TEST(Queue, cant_create_queue_with_zero_length) 
+{
+    ASSERT_ANY_THROW(Queue<int> v(0));
+}
+
+TEST(Queue, cant_create_too_large_queue) 
+{
+    ASSERT_ANY_THROW(Queue<int> v(MaxSize + 100));
+}
+
+TEST(Queue, cant_enqueue_to_full_queue) 
+{
+    Queue<int> v(2);
+    v.enqueue(1);
+    v.enqueue(2);
+    ASSERT_ANY_THROW(v.enqueue(4));
+}
+
+TEST(Queue, cant_dequeue_of_clear_queue)
+{
+    Queue<int> v(2);
+    ASSERT_ANY_THROW(v.dequeue());
+}
+
+TEST(Queue, can_dequeue_of_queue)
+{
+    Queue<int> v(2);
+    v.enqueue(1);
+    ASSERT_NO_THROW(v.dequeue());
+}
+
+TEST(Queue, can_dequeue_of_queue_about_one)
+{
+    Queue<int> v(5);
+    v.enqueue(4);
+    EXPECT_EQ(4, v.dequeue());
+}
+
+TEST(Queue, can_dequeue_of_queue_about_two)
+{
+    Queue<int> v(5);
+    v.enqueue(4);
+    v.enqueue(7);
+    EXPECT_EQ(4, v.dequeue());
+}
+
+TEST(Queue, can_dequeue_of_queue_about_three)
+{
+    Queue<int> v(5);
+    v.enqueue(4);
+    v.enqueue(1);
+    v.enqueue(77);
+    EXPECT_EQ(4, v.dequeue());
+}
+
+TEST(Queue, can_dequeue_twice_of_queue_about_three)
+{
+    Queue<int> v(5);
+    v.enqueue(4);
+    v.enqueue(1);
+    v.enqueue(77);
+    v.dequeue();
+    EXPECT_EQ(1, v.dequeue());
+}
+
+TEST(Queue, IsEmpty_false_of_full_queue) 
+{
+    Queue<int> v(2);
+    v.enqueue(4);
+    v.enqueue(4);
+    ASSERT_FALSE(v.IsEmpty());
+}
+
+TEST(Queue, IsEmpty_true_of_clear_queue) 
+{
+    Queue<int> v(2);
+    ASSERT_TRUE(v.IsEmpty());
+}
+
+TEST(Queue, IsEmpty_false_of_any_queue)
+{
+    Queue<int> v(2);
+    v.enqueue(4);
+    ASSERT_FALSE(v.IsEmpty());
+}
+
+TEST(Queue, IsFull_true_of_full_queue) 
+{
+    Queue<int> v(2);
+    v.enqueue(4);
+    v.enqueue(4);
+    ASSERT_TRUE(v.IsFull());
+}
+
+TEST(Queue, IsFull_false_of_clear_queue)  
+{
+    Queue<int> v(2);
+    ASSERT_FALSE(v.IsFull());
+}
+
+TEST(Queue, IsFull_false_of_any_queue)  
+{
+    Queue<int> v(2);
+    v.enqueue(4);
+    ASSERT_FALSE(v.IsFull());
+}
